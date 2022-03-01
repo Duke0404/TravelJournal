70618ab3d1022853dc8c13883f0f62352d93e7f6
Duke0404 <devansh0404@gmail.com>
2022-03-01 17:40:23 +0100

    Added dataSource

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css"
 
+import dataSource from "../../data/dataSource"
+
 import Titlebar from "../Titlebar/Titlebar"
+import LocationCard from "../LocationCard/LocationCard"
+
+const locationCardArray = dataSource.map(val => val = <LocationCard key={val.id} {...val} />)
 
 const App = () =>
-    <Titlebar />
+    <div id="all-container">
+        <Titlebar />
+        <div id="locations-container">
+            {locationCardArray}
+        </div>
+    </div>
 
 export default App
---
+++ src/data/dataSource.js
@@ -0,0 +1,11 @@
+const dataSource = [
+    {
+        id: 1,
+        name: "Auschwitz-Birkenau",
+        img: "Auschwitz-Birkenau.jpg",
+        link: "https://maps.google.com",
+        date: "20-02-2022"
+    }
+]
+
+export default dataSource
