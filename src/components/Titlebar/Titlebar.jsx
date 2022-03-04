373e97264bb40513ad7f1d018e73d8ba5ed124bc
Duke0404 <devansh0404@gmail.com>
2022-03-04 07:53:01 +0100

    Fixed Titlebar image

--- src/components/Titlebar/Titlebar.jsx
+++ src/components/Titlebar/Titlebar.jsx
@@ -2,7 +2,7 @@ import "./Titlebar.css"
 
 const Titlebar = () =>
     <header id="titlebar-container">
-        <img id="title-logo" src={`${process.env.PUBLIC_URL}/favicon.svg`} title ="Icon" alt="Icon" />
+        <img id="title-logo" src={`${process.env.PUBLIC_URL}/favicons/favicon.svg`} title ="Icon" alt="Icon" />
         <h1>My Travel Journal</h1>
     </header>
 
